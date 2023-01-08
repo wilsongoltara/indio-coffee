b2b4e9ad4b195e3e77d4772e6499d2d01046a9c5
wilsongoltara <wilson.goltara15@gmail.com>
2023-01-08 19:49:59 -0300

    [REFACTOR]: Add font by next/font

---
+++ lib/fonts.ts
@@ -0,0 +1,18 @@
+export const fonts = [
+  {
+    path: './SoDoSans-Regular.woff2',
+    weight: '400',
+  },
+  {
+    path: './SoDoSans-SemiBold.woff2',
+    weight: '400',
+  },
+  {
+    path: './SoDoSans-Bold.woff2',
+    weight: '700',
+  },
+  {
+    path: './SoDoSans-Light.woff2',
+    weight: '700',
+  },
+];
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import Layout from '@components/Layout';
 import type { AppProps } from 'next/app';
+import localFont from '@next/font/local';
 import '../styles/globals.css';
+
+const myFont = localFont({ src: '../public/font/SoDoSans-Bold.woff2' });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <main className={myFont.className}>
+        <Component {...pageProps} />
+      </main>
     </Layout>
   );
 }
