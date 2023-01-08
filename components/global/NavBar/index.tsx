2b177e1a74ead6070d2a8db200dc6a585ecde197
wilsongoltara <wilson.goltara15@gmail.com>
2023-01-08 12:18:14 -0300

    [FEAT]: add component NavBar

---
+++ components/global/NavBar/index.tsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>link 1</li>
+        <li>link 1</li>
+        <li>link 1</li>
+      </ul>      
+    </nav>
+  );
+};
+
+export default NavBar;
