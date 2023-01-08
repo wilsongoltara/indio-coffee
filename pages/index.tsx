77e3f21cf08cae02137401af2a9d93c346fec052
wilsongoltara <wilson.goltara15@gmail.com>
2023-01-08 11:40:04 -0300

    [FEAT]: add tailwindcss and config

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,8 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main>
-        <h1>Hello World!</h1>
+      <main className='flex items-center justify-center h-screen w-auto'>
+        <h1 className='text-3xl font-bold'>Indio Coffee Company!</h1>
       </main>
     </>
   );
