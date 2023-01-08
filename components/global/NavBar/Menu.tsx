9d789e027ebef6a7243a0946827699e0a33ef4cc
wilsongoltara <wilson.goltara15@gmail.com>
2023-01-08 13:32:48 -0300

    [FEAT]: Add component Menu

---
+++ components/global/NavBar/Menu.tsx
@@ -0,0 +1,13 @@
+const Menu = () => {
+  return (
+    <menu>
+      <ul className="flex w-auto">
+        <li className="p-5">link 1</li>
+        <li className="p-5">link 2</li>
+        <li className="p-5">link 3</li>
+      </ul>
+    </menu>
+  );
+};
+
+export default Menu;
