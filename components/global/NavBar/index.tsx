de4561bfebc9fb72f003f0784a596c789f5c0f43
wilsongoltara <wilson.goltara15@gmail.com>
2023-01-09 13:19:59 -0300

    [FEAT]: Add lazy dynamic for logo

--- components/global/NavBar/index.tsx
+++ components/global/NavBar/index.tsx
@@ -1,16 +1,18 @@
-import Logo from '@assets/Logo';
-import { links } from '@lib/links';
+import { linksMenu } from '@lib/links';
+import dynamic from 'next/dynamic';
 import ButtonsBar from './ButtonsBar';
 import Menu from './Menu';
+
+const DynamicLogo = dynamic(() => import('@assets/Logo'));
 
 export default function NavBar() {
   return (
     <nav className='flex justify-between items-center h-16 ml-5 md:h-20 lg:h-24 lg:ml-10'>
       <div className='flex items-center justify-between w-full md:w-auto'>
-        <Logo />
-        <Menu links={links}/>
+        <DynamicLogo />
+        <Menu links={linksMenu} />
       </div>
       <ButtonsBar />
     </nav>
   );
-};
+}
